75ba6829cdd32842f0abb38519574b7b02b4201f
Rayan <khanrayan@pm.me>
2025-10-26 17:23:50 -0400

    Created "Movie" Table and added missing DirectorID and and ActorID.

--- MovieCatalogue.sql
+++ MovieCatalogue.sql
@@ -1,13 +1,15 @@
 CREATE DATABASE MovieCatalogue;
 USE MovieCatalogue;
+
 CREATE TABLE Genre (
     INT IDENTITY(1,1) PRIMARY KEY,
     GenreName VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE Director (
-    INT IDENTITY(1,1) PRIMARY KEY,
-    DirectorName VARCHAR(30) NOT NULL
+    DirectorID INT IDENTITY(1,1) PRIMARY KEY,
+    FirstName VARCHAR(30) NOT NULL,
+    LastName VARCHAR(30) NOT NULL,
 );
 
 CREATE TABLE Rating (
@@ -16,11 +18,24 @@ CREATE TABLE Rating (
 );
 
 CREATE TABLE Actor  (
-    INT IDENTITY(1,1) PRIMARY KEY,
+    ActorID INT IDENTITY(1,1) PRIMARY KEY,
     FirstName VARCHAR(30) NOT NULL,
     LastName VARCHAR(30) NOT NULL, 
     BirthDate DATE NOT NULL
 );
 
+CREATE TABLE Movie (
+    MovieID INT IDENTITY(1,1) PRIMARY KEY,
+    Title VARCHAR(128) NOT NULL,
+    ReleaseDate DATE NOT NULL,
+    -- Foreign Keys
+    GenreID INT NOT NULL,
+    DirectorID INT NOT NULL,
+    RatingID INT NOT NULL,
+    -- Constraints
+    CONSTRAINT FK_Movie_Genre FOREIGN KEY (GenreID) REFERENCES Genre(GenreID),
+    CONSTRAINT FK_Movie_Director FOREIGN KEY (DirectorID) REFERENCES Director(DirectorID),
+    CONSTRAINT FK_Movie_Rating FOREIGN KEY (RatingID) REFERENCES Rating(RatingID)
+);
 
 
